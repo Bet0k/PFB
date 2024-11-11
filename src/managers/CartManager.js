4a5d3436963fefec6c5cea1a63499572ccf6483d
Beto Koll <albertokoll97@gmail.com>
2024-11-10 22:23:07 -0300

    cart-logic

--- src/managers/CartManager.js
+++ src/managers/CartManager.js
@@ -1,8 +1,7 @@
 import { readJsonFile, writeJsonFile } from "../utils/fileHandler.js";
 import paths from "../utils/paths.js";
 import { generateId } from "../utils/collectionHandler.js";
-//import { covertToBool } from "../utils/converter.js";
-import ErrorManager from "./ErrorMangaer.js";
+import ErrorManager from "./ErrorManager.js";
 
 export default class CartManager{
     #jsonFileName;
@@ -16,14 +15,23 @@ export default class CartManager{
         this.#carts = await this.getAll();
         const cartFound = this.#carts.find((item) => item.id === Number(id));
         if(!cartFound){
-            throw new Error("No se encontró ningún carto con el ID ingesado.", 404);
+            throw new ErrorManager("No se encontró ningún carrito con el ID ingesado.", 404);
         }
+        return cartFound;
     }
 
     async getAll() {
         try {
             this.#carts = await readJsonFile(paths.files, this.#jsonFileName);
-            return this.#carts;
+            return this.#carts.map((cart) => ({
+                id: cart.id,
+                products: cart.products.map((cartProduct) => {
+                    return {
+                        product: cartProduct.product,
+                        quantity: cartProduct.quantity,
+                    };
+                }),
+            }));
         } catch (error) {
             throw new ErrorManager(error.message, error.code);
         }
@@ -32,7 +40,10 @@ export default class CartManager{
     async getOneById(id) {
         try {
             const cartFound = await this.$findOneById(id);
-            return cartFound;
+            return {
+                id: cartFound.id,
+                cartDetails: cartFound.products,
+            };
         } catch (error) {
             throw new ErrorManager(error.message, error.code);
         }
@@ -40,62 +51,53 @@ export default class CartManager{
 
     async insertOne(data) {
         try {
-            const { title, status, stock } = data;
-            if(!title || status === undefined || status === null|| !stock){
-                throw new ErrorManager("No se ingresaron los datos necesarios para crear el carto.", 400);
-            }
-
+            const availableProducts = await readJsonFile(paths.files, "products.json");
+            const products = data?.products?.map((item) => {
+                const productExists = availableProducts.find(
+                    (availableProduct) => availableProduct.id === Number(item.product),
+                );
+                if (!productExists) {
+                    throw new ErrorManager(`Producto con ID ${item.product} no existe.`, 404);
+                }
+                return { product: productExists.id, quantity: 1 };
+            });
             const cart = {
-                id:  generateId(await this.getAll()),
-                title,
-                status,
-                stock,
+                id: generateId(await this.getAll()),
+                products: products,
             };
-
             this.#carts.push(cart);
             await writeJsonFile(paths.files, this.#jsonFileName, this.#carts);
-
             return cart;
         } catch (error) {
             throw new ErrorManager(error.message, error.code);
         }
     }
 
-    // async updateOneById(id, data) {
-    //     try {
-    //         const { title, status, stock } = data;
-    //         const cartFound = this.getOneById(id);
-    //         //const cartFound = await this.$findOneById(id);
+    updateCart = async (id, productId, quantity) => {
+        try {
+            const availableProducts = await readJsonFile(paths.files, "products.json");
+            const productExists = availableProducts.find(
+                (availableProduct) => availableProduct.id === Number(productId),
+            );
+            if (!productExists) {
+                throw new ErrorManager(`Producto con ID ${productId} no existe.`, 404);
+            }
+            const cartFound = await this.$findOneById(id);
+            const productIndex = cartFound.products.findIndex(
+                (item) => item.product === Number(productId),
+            );
 
-    //         const cart = {
-    //             id: cartFound.id,
-    //             title: title || cartFound.title,
-    //             status: status ? covertToBool(status) : cartFound.status,
-    //             stock: stock ? Number(stock) : cartFound.stock,
-    //         };
-
-    //         const index = this.#carts.findIndex(() => item.id === Number(id));
-    //         this.#carts[index] = cart;
-    //         await writeJsonFile(paths.files, this.#jsonFileName, this.#carts);
-
-    //         return cart;
-    //     } catch (error) {
-    //  throw new ErrorManager(error.message, error.code);
-    //     }
-    // }
-
-    // async deleteOneById(id) {
-    //     try {
-    //         await this.getOneById(id);
-    //         //const cartFound = await this.$findOneById(id);
-
-    //         const index = this.#carts.findIndex(() => item.id === Number(id));
-    //         this.#carts.splice[index, 1];
-    //         await writeJsonFile(paths.files, this.#jsonFileName, this.#carts);
-
-    //         return cart;
-    //     } catch (error) {
-    //    throw new ErrorManager(error.message, error.code);
-    //     }
-    // }
+            if (productIndex >= 0) {
+                cartFound.products[productIndex].quantity += quantity;
+            } else {
+                cartFound.products.push({ product: Number(productId), quantity: quantity });
+            }
+            const index = this.#carts.findIndex((item) => item.id === Number(id));
+            this.#carts[index] = cartFound;
+            await writeJsonFile(paths.files, this.#jsonFileName, this.#carts);
+            return cartFound;
+        } catch (error) {
+            throw new ErrorManager(error.message, error.code);
+        }
+    };
 }
